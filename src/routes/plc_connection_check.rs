041577a584ecf1307addceb06b122bfade982289
Luka Koridze <lukakoridze13@gmail.com>
2024-07-23 09:56:37 +0400

    Enable Address, Rack, Slot in request query

--- src/routes/plc_connection_check.rs
+++ src/routes/plc_connection_check.rs
@@ -1,19 +1,47 @@
 use axum::{
-    response::{IntoResponse, Response},
+    extract::Query,
     http::StatusCode,
+    response::{IntoResponse, Json},
 };
 use snap7_rs::S7Client;
+use serde::{Deserialize, Serialize};
 
-pub async fn plc_connection_check() -> Response {
+#[derive(Serialize, Deserialize)]
+pub struct PLCConnectionCheckQuery {
+    address: String,
+    rack: i32,
+    slot: i32,
+}
+
+#[derive(Serialize)]
+pub struct PLCConnectionCheckResponse {
+    address: String,
+    rack: i32,
+    slot: i32,
+    message: String,
+}
+
+pub async fn plc_connection_check(Query(query): Query<PLCConnectionCheckQuery>) -> impl IntoResponse {
     let client = S7Client::create();
-    match client.connect_to("192.168.0.1", 0, 2) {
+    let connection_result = client.connect_to(&query.address, query.rack, query.slot);
+
+    let (status_code, message) = match connection_result {
         Ok(_) => (
             StatusCode::OK,
-            "Connected to PLC".to_string()
-        ).into_response(),
+            "Connected to PLC".to_string(),
+        ),
         Err(e) => (
             StatusCode::SERVICE_UNAVAILABLE,
-            format!("Error connecting to PLC: {:?}", e)
-        ).into_response(),
-    }
+            format!("Error connecting to PLC: {:?}", e),
+        ),
+    };
+
+    let response = PLCConnectionCheckResponse {
+        address: query.address.clone(),
+        rack: query.rack,
+        slot: query.slot,
+        message,
+    };
+
+    (status_code, Json(response)).into_response()
 }
